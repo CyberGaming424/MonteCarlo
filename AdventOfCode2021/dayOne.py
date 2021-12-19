f353683b03395c3b5f7f71d73121453af49dd876
BuildTools <unconfigured@null.spigotmc.org>
2021-12-18 21:41:59 -0500

    Day One of advent

--- AdventOfCode2021/dayOne.py
+++ AdventOfCode2021/dayOne.py
@@ -14,5 +14,16 @@
 # Part Two
 with open('AdventOfCode2021/dayOne.txt') as dayOne:
     lines  = dayOne.readlines()
-    increase = 0
-    
+    increased = 0
+    previousSum = 0
+    for x in range(len(lines)):
+        if(x < (len(lines)-2)):
+            currentSum = int(lines[x])+int(lines[x+1])+int(lines[x+2])
+            if(previousSum != 0):
+                if(currentSum > previousSum):
+                    increased += 1
+            previousSum = currentSum
+    print(increased)
+    dayOne.close
+        
+
